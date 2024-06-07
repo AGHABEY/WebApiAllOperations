93a69bdb4cce382ce7ff194101eef8c879fe127a
rafetaghabeyli <rafet.agabeyli98@gmail.com>
2024-06-08 02:34:47 +0400

    Fixes added

---
+++ WebApiAllOperations/Dtos/Comment/UpdateCommentRequestDto.cs
@@ -0,0 +1,9 @@
+using System.Security;
+
+namespace WebApiAllOperations.Dtos.Comment;
+
+public class UpdateCommentRequestDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+}
